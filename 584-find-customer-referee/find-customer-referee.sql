98e51742196edcfc0c92980bee42fa823aeefb2b
Abdulmoiz <rashidabdulmoiz55@gmail.com>
2025-10-18 12:43:50 +0500

    Time: 624 ms (24.03%) | Memory: 0B (100.00%) - LeetSync

--- 584-find-customer-referee/find-customer-referee.sql
+++ 584-find-customer-referee/find-customer-referee.sql
@@ -1,4 +1,4 @@
 /* Write your T-SQL query statement below */
-select name 
-from customer 
-where referee_id is null or referee_id !=2
+select c.name 
+from customer c 
+where c.referee_id is null or c.referee_id !=2
